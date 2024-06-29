fb7230498d1504a20d6ce745c1e926a5ef70ca4c
k4in <>
2024-06-29 19:52:12 +0200

    theming

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainPage({ children }: { children: ReactNode }) {
   return (
     <>
       {/* TODO!! Why is it not changing color when changing modes? */}
-      <header className="bg-foreground dark:bg-background dark:text-foreground shrink-0 border-b dark:border-b-primary-foreground border-b-primary h-16 px-10 flex items-center justify-between sticky top-0 z-10">
+      <header className="bg-background shrink-0 border-b border-b-foreground h-16 px-10 flex items-center justify-between sticky top-0 z-10">
         <div>[LOGO]</div>
         <div className="flex gap-6">
           <ModeToggle />
@@ -18,7 +18,7 @@ export default function MainPage({ children }: { children: ReactNode }) {
         </div>
       </header>
       <main className="flex-1 p-10">{children}</main>
-      <footer className="shrink-0 border-t dark:border-t-primary-foreground border-t-primary h-10 flex justify-end items-center px-10">
+      <footer className="shrink-0 border-t border-t-foreground h-10 flex justify-end items-center px-10">
         <span className="text-xs">&copy; thek4in 2024</span>
       </footer>
     </>
